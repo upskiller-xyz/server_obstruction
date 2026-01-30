c15034114e9a6a4242a1811537b5427239ca75a3
STASYA00 <0.0stasya@gmail.com>
2026-01-30 22:50:41 +0100

    fix: add missing package; heighten the min/max borders

--- src/utils/settings.py
+++ src/utils/settings.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 
 class Settings:
-    max_horizontal_distance:float = 3 # limit for zenith angle
-    min_horizontal_distance:float = 2.5 # limit for horizon angle
+    max_horizontal_distance:float = 8 # limit for zenith angle
+    min_horizontal_distance:float = 6 # limit for horizon angle
     max_vertical_normal_z = 0.5 # declination to determine vertical surfaces
     max_angle_degrees:float = 80 # obstruction angle limit
